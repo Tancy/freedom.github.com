fff16a817b1656148f7a465c8f41bd2f25e03530
Tancy <ronntx@gmail.com>
2010-07-17 13:52:58 +0800

    init commit

---
+++ doc/guide.php
@@ -0,0 +1,57 @@
+
+<div class="siderbar">
+	<div id="sider-nav" class="navlist">
+		<div class="navgroup">
+			<h4>Cubee CSS Framework</h4>
+			<ul>
+				<!--在这里添加菜单项-->
+				<li><a class="ajax-click" href="#cssreset">&bull; Reset.css</a></li>
+				<li><a class="ajax-click" href="#cssfont">&bull; Font.css</a></li>
+				<li><a class="ajax-click" href="#cssgrid">&bull; Grid.css</a></li>
+				<!--li><a class="ajax-click" href="">&bull; common.css</a></li-->
+			</ul>
+		</div><!-- .navgroup -->
+		<div class="navgroup">
+			<h4>Cubee Widgets</h4>
+			<ul>
+				<!--在这里添加菜单项-->
+				<li><a class="ajax-click" href="#cubee-base">&bull; Y.Base 常用工具</a></li>
+				<li><a class="ajax-click" href="#tip">&bull; Y.Tip 提示信息</a></li>
+				<li><a class="ajax-click" href="#box">&bull; Y.Box 弹出层</a></li>
+				<li><a class="ajax-click" href="#calendar">&bull; Y.Calendar 日历控件</a></li>
+				<li><a class="ajax-click" href="#simple-calendar">&bull; Y.Scalendar 简单日历</a></li>
+				<li><a class="ajax-click" href="#pagination">&bull; Y.Pagination 分页</a></li>
+				<li><a class="ajax-click" href="#simple-page">&bull; Y.Spage 简单分页列表</a></li>
+				<li><a class="ajax-click" href="#slide">&bull; Y.Slide Tab+Slide</a></li>
+				<li><a class="ajax-click" href="#postip">&bull; Y.Postip 相对提示</a></li>
+				<li><a class="ajax-click" href="#datecascade">&bull; Y.DateCascade 级联日期控件</a></li>
+				<li><a class="ajax-click" href="#checkall">&bull; Y.Checkall checkbox全选反选</a></li>
+				<li><a class="ajax-click" href="#tip-input">&bull; Y.tipInput 文本输入框</a></li>
+				<li><a class="ajax-click" href="#floatip">&bull; Y.Floatip 浮动提示</a> <sup>beta</sup></li>
+				<li><a class="ajax-click" href="#ticker">&bull; Y.Ticker ticker滚动</a> <sup>beta</sup></li>
+				<li><a class="ajax-click" href="#cutter">&bull; Y.Cutter Cutter图片展示</a> <sup>beta</sup></li>
+				<li><del><a class="ajax-click" href="#rankinglist">&bull; Y.RankingList 排行榜</a></del></li>
+				<li><del><a class="ajax-click" href="#slideshow">&bull; Y.Slideshow 幻灯</a></del></li>
+			</ul>
+		</div><!-- .navgroup -->
+		<div class="navgroup">
+			<h4>Cubee Modules</h4>
+			<ul>
+				<!--在这里添加菜单项-->
+				<li><a class="ajax-click" href="#template">&bull; Template 通用HTML模板</a></li>
+				<li><a class="ajax-click" href="#using-css3">&bull; 适当使用CSS3</a></li>
+				<li><del><a class="ajax-click" href="#vote">&bull; vote 投票</a></del></li>
+			</ul>
+		</div><!-- .navgroup -->
+		<div class="navgroup">
+			<h4>Cubee Patterns</h4>
+			<ul>
+				<!--在这里添加菜单项-->
+				<li><a class="ajax-click" href="#cross-domain-autoheight">&bull; 跨域高度自适应</a></li>
+				<li><a class="ajax-click" href="#sandbox-js">&bull; Sandbox</a></li>
+				<li><a class="ajax-click" href="#combo">&bull; Combo脚本</a></li>
+				<li><a class="ajax-click" href="#cdn-dev">&bull; CDN开发环境配置</a></li>
+			</ul>
+		</div><!-- .navgroup -->
+	</div>
+</div>
---
+++ doc/page/index.php
@@ -0,0 +1,24 @@
+<div class="tit">Cubee 手册</div>
+<p class="author">Cubee是淘宝北京UED开发并维护的UI库,这里是代码级的开发者手册。</p>
+<h2><strong>What's Cubee?</strong></h2>
+<!--div class="demo">
+	<a class="button" href="../src/box/demo/demo-box.html">演示示例 DEMO</a>
+</div-->
+<div class="dec">
+	<p>
+		Cubee是一套整洁实用的UI控件库，他包括css、widgets、modules和patterns，可以用来快捷方便的构建富交互的web应用，配合yui3使用效果更佳。目前cubee的控件已应用在淘宝网的各个角落……
+	</p>
+</div>
+<h2><strong>维护</strong></h2>
+<ul class="dec">
+	<li>WD：蒋吉兵(花名乐渔):中国雅虎/淘宝网 前端工程师</li>
+	<li>WD：李晶(花名拔赤):中国雅虎/淘宝网 前端工程师</li>
+</ul>
+<h2><strong>Cubee Introduction</strong></h2>
+<ul class="dec">
+	<li>Cubee包含一套基于yui3的widget控件库，可以协助你更方便快捷的搭建基于yui3的富交互web应用。</li>
+	<li>Cubee项目最初源自淘宝北京UED的一个团队项目，包含常见的团队开发解决方案。</li>
+	<li>Cubee给出一套css/js框架思路，开发者可以方便对其进行扩展和二次开发。</li>
+	<li>Cubee基于<a href="http://www.opensource.org/licenses/mit-license.php">MIT协议</a>，你可以方便的参与开发。</li>
+	<li>你可能会对<a href="http://developer.yahoo.com/yui/3">yui3</a>更感兴趣。在进行cubee开发之前，请详细阅读yui3文档。</li>
+</ul>
